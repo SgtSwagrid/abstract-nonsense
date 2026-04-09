16ee675dcafa89e89d15a4df7551799f417b0fee
Alec Dorrington <alecdorrington@gmail.com>
2026-04-09 14:54:39 +0200

    Added Bijection.scala.

---
+++ src/main/scala/function/Bijection.scala
@@ -0,0 +1,73 @@
+package io.github.sgtswagrid.nonsense
+package function
+
+import io.github.sgtswagrid.nonsense.function.Bijection.<=>
+
+/**
+  * A bijection is an invertible function. A function `f` is invertible if and
+  * only if it is both:
+  *
+  *   - Injective: `∀ i₁, i₂ ∈ In . i₁ ≠ i₂ ⇒ f(i₁) ≠ f(i₂)`
+  *   - Surjective: `∀ o ∈ Out ∃ i ∈ In . f(i) = o`
+  *
+  * Can be used as an ordinary function `A => B`, or otherwise call [[invert]]
+  * to obtain the inverse bijection.
+  *
+  * @tparam In
+  *   The input to the function.
+  *
+  * @tparam Out
+  *   The output of the function.
+  */
+trait Bijection[In, Out] extends (In => Out):
+
+  /**
+    * Provides the inverse of this bijective function.
+    *
+    * @note
+    *   This operator is an involution, meaning that applying is twice will
+    *   return the original value: `this.invert.invert == this`.
+    *
+    * @note
+    *   The existence of this operator suffices to prove the existence of an
+    *   inverse. Safety is assured.
+    *
+    * @note
+    *   Whether the inverse is computed by this operator, or is simply known in
+    *   advance and returned as-is, depends on the underlying type.
+    */
+  def invert: Out <=> In
+
+object Bijection:
+
+  /** Alias for [[Bijection]]. */
+  infix type <=>[In, Out] = Bijection[In, Out]
+
+  /**
+    * A [[Bijection]] constructed by separately and explicitly providing the
+    * [[forward]] and [[backward]] versions of a function.
+    *
+    * @param forward
+    *   The forward direction of the represented function.
+    *
+    * @param backward
+    *   The backward direction of the represented function.
+    *
+    * @tparam In
+    *   The input to the function.
+    *
+    * @tparam Out
+    *   The output of the function.
+    *
+    * @note
+    *   The caller is responsible for ensuring that [[forward]] and [[backward]]
+    *   are indeed each other's inverse. No checks are made, and this property
+    *   is henceforth assumed to hold.
+    */
+  class Explicit[In, Out]
+    (forward: In => Out, backward: Out => In)
+    extends (In <=> Out):
+
+    override final inline def apply(value: In): Out = forward(value)
+
+    override final inline def invert: Out <=> In = Explicit(backward, forward)
--- src/main/scala/function/InvertibleFunction.scala
+++
@@ -1,4 +0,0 @@
-package io.github.sgtswagrid.nonsense
-package function
-
-infix trait InvertibleFunction[A, B] extends (A => B)
